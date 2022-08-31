d08ff96951a2f3ced27a1e325da6294f2e9bd01d
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2022-08-31 17:05:02 +0200

    Increased test coverage and fixed bug in calculation of homogeneity scores

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,4 +1,11 @@
 
+
+#----Start tests----------------------------------------------------------------
+test_that("is_valid() returns a logical scalar", {
+    v <- is_valid("echo")
+    expect_equal(class(v), "logical")
+})
+
 test_that("busco_is_installed() returns logical values", {
   l <- busco_is_installed()
   expect_equal(class(l), "logical")
--- tests/testthat/test-visualization.R
+++ tests/testthat/test-visualization.R
@@ -1,12 +1,12 @@
 
-#----Load data----
+#----Load data------------------------------------------------------------------
 data(tree)
 data(og)
 dir <- system.file("extdata", package = "cogeqc")
 stats_list <- read_orthofinder_stats(dir)
 
 
-#----Start tests----
+#----Start tests----------------------------------------------------------------
 test_that("plot_species_tree() returns a ggplot object with a species tree", {
     p <- plot_species_tree(tree)
     expect_true("ggplot" %in% class(p))
@@ -30,6 +30,7 @@ test_that("plot_duplications() returns a ggplot object", {
 test_that("plot_orthofinder_stats() returns a ggplot object", {
     p <- plot_orthofinder_stats(tree, stats_list)
     expect_true("ggplot" %in% class(p))
+    expect_error(plot_orthofinder_stats())
 })
 
 test_that("plot_og_overlap() returns a ggplot object", {
@@ -39,6 +40,22 @@ test_that("plot_og_overlap() returns a ggplot object", {
 
 test_that("plot_og_sizes() returns a ggplot object", {
     p <- plot_og_sizes(og, log = TRUE)
+    p2 <- plot_og_sizes(og, log = FALSE)
+
+    # Create fake orthogroup data frame with >20 species for testing
+    og_sim <- data.frame(
+        Orthogroup = paste0("OG", 0001:0005),
+        Species = rep(paste0("sp", 1:25), 5),
+        Gene = c(
+            rep("gene1", 50), rep("gene2", 50),
+            rep("gene3", 25)
+        )
+    )
+    p3 <- plot_og_sizes(og_sim)
+
     expect_true("ggplot" %in% class(p))
+    expect_true("ggplot" %in% class(p2))
+    expect_true("ggplot" %in% class(p3))
+
 })
 
